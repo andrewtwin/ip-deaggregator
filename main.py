f294cc834f1b98c540973be26d55d97a9a7e7373
Andrew Twin <andrewtwin@gmail.com>
2021-11-26 07:42:31 +1100

    making code more readable, further testing

--- main.py
+++ main.py
@@ -2,31 +2,29 @@ import ipaddress
 
 def main():
    supernet = ipaddress.ip_network('10.0.0.0/16')
-   skip_networks = [
+   gap_subnets = [
        ipaddress.ip_network('10.0.0.0/24'),
-       ipaddress.ip_network('10.0.3.0/24')
+       ipaddress.ip_network('10.0.3.0/25'),
+       ipaddress.ip_network('10.0.90.0/26')
        ]
 
-   print(f"Finding subnets of {supernet} which don't include {skip_networks}")
-   check_subnets(supernet, skip_networks)
+   print(f"Finding subnets of {supernet} which don't include {', '.join(str(i) for i in gap_subnets)}")
+   check_subnets(supernet, gap_subnets)
 
-def check_subnets(supernet, skip_networks):
-    for network in supernet.subnets(1):
-       #print(f"Considering {network}")
-       skip = False
-       max_gap = supernet.prefixlen
-       for gap in skip_networks:
-           #print(f"Checking {gap}")
-           if gap.prefixlen > max_gap:
-               max_gap = gap.prefixlen
-               #print(f"New max_gap size {max_gap}")
-           if gap.subnet_of(network) or network.subnet_of(gap):
-               skip = True
+def check_subnets(supernet, gap_subnets):
+    for subnet in supernet.subnets(1):
+       unsuitable_subnet = False
+       max_gap_size = supernet.prefixlen
+       for gap in gap_subnets:
+           if gap.prefixlen > max_gap_size:
+               max_gap_size = gap.prefixlen
+           if gap.subnet_of(subnet) or subnet.subnet_of(gap):
+               unsuitable_subnet = True
         
-       if not skip:
-            print(f"{network}")
-       elif network.prefixlen < max_gap:
-           check_subnets(network, skip_networks)
+       if not unsuitable_subnet:
+            print(f"{subnet}")
+       elif subnet.prefixlen < max_gap_size:
+           check_subnets(subnet, gap_subnets)
 
 if __name__ == "__main__":
     main()
