28a06d4147bd09fc21d45fe15205205d6d8fd038
Andrew Twin <andrewtwin@gmail.com>
2021-11-25 22:24:27 +1100

    this is hurting my brain

--- main.py
+++ main.py
@@ -4,25 +4,26 @@ def main():
    supernet = ipaddress.ip_network('10.0.0.0/16')
    skip_networks = [
        ipaddress.ip_network('10.0.0.0/24'),
-       ipaddress.ip_network('10.0.128.0/24')
+       ipaddress.ip_network('10.0.1.0/24')
        ]
 
    print(f"Finding subnets of {supernet} which don't include {skip_networks}")
-   get_subnets(supernet, skip_networks)
+   check_subnets(supernet, skip_networks)
 
-def get_subnets(supernet, skip_networks):
+def check_subnets(supernet, skip_networks):
     for network in supernet.subnets(1):
-        contains_gap = False
+        skip_network = False
+        skip_subnet = False
         for gap in skip_networks:
-            if gap.subnet_of(network):
-                contains_gap = True
-            else:    
-                if gap.prefixlen > network.prefixlen:
-                    break
-        if not contains_gap :
+            if gap.subnet_of(network): 
+                skip_network = True
+            if gap.prefixlen > network.prefixlen:
+                skip_subnet = True
+        
+        if not (skip_network or skip_subnet):
                 print(f"{network}")
         else:
-            get_subnets(network, skip_networks)
+            check_subnets(network, skip_networks)
 
 if __name__ == "__main__":
     main()
